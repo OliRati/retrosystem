1a14276fee6092311f7ac5c053d245f198d14ae6
Olivier RATIEVILLE <olivier.ratieville@gmail.com>
2024-11-07 23:22:56 +0100

    Add Menus and Windows

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,238 @@
-/* Beautyfull JavaScript */
+/* Currently shown menu */
+
+let menuEarthShown = false;
+let menuConfigShown = false;
+let menuViewShown = false;
+let menuAboutShown = false;
+
+/* Selected item in config menu - default is Ordered transition */
+
+let selectedChecklist = 2;
+
+const background = document.getElementById("background");
+
+
+/* Add window frame decoration */
+
+function addWindowFrame(title, closebutton = false) {
+    const div = document.createElement('div');
+    div.className = 'windowbar';
+    div.innerHTML = `            
+                <div class="windowbarstart">
+                    <div class="windowbardecoration"></div>
+                    <div class="windowbardecoration"></div>
+                    <div class="windowbardecoration"></div>
+                    <div class="windowbardecoration"></div>
+                    <div class="windowbardecoration"></div>
+                </div>
+                <div class="windowbarcube">
+                </div>
+                <div class="windowtitleframe">
+                    <div class="windowbarend">
+                        <div class="windowbardecoration"></div>
+                        <div class="windowbardecoration"></div>
+                        <div class="windowbardecoration"></div>
+                        <div class="windowbardecoration"></div>
+                        <div class="windowbardecoration"></div>
+                    </div>
+                    <div class="windowtitle">`+ title + `</div>
+                    <div class="windowbarend">
+                        <div class="windowbardecoration"></div>
+                        <div class="windowbardecoration"></div>
+                        <div class="windowbardecoration"></div>
+                        <div class="windowbardecoration"></div>
+                        <div class="windowbardecoration"></div>
+                    </div>
+                </div>
+            `;
+    if (closebutton) {
+        div.innerHTML += `<div class="windowbarclose"></div>`;
+    }
+    return div;
+}
+
+function adjustMenuStates(i) {
+    if ((i != 0) && menuEarthShown) {
+        menuEarthShown = false;
+        document.getElementById("earthbox").style.display = "none";
+    }
+
+    if ((i != 1) && menuConfigShown) {
+        menuConfigShown = false;
+        document.getElementById("configbox").style.display = "none";
+    }
+
+    if ((i != 2) && menuViewShown) {
+        menuViewShown = false;
+        document.getElementById("viewbox").style.display = "none";
+    }
+
+    if ((i != 3) && menuAboutShown) {
+        menuAboutShown = false;
+        document.getElementById("aboutbox").style.display = "none";
+    }
+}
+
+var menuEarth = document.getElementById("menuearth");
+menuEarth.addEventListener("click", () => {
+    menuEarthShown = !menuEarthShown;
+    if (menuEarthShown) {
+        document.getElementById("earthbox").style.display = "block";
+        document.getElementById("earthbox").style.animation = "movemenu .4s forwards";
+    }
+    else
+        document.getElementById("earthbox").style.display = "none";
+
+    adjustMenuStates(0);
+});
+
+var menuConfig = document.getElementById("menuconfig");
+menuConfig.addEventListener("click", () => {
+    menuConfigShown = !menuConfigShown;
+    if (menuConfigShown) {
+        document.getElementById("configbox").style.display = "block";
+        document.getElementById("configbox").style.animation = "movemenu .4s forwards";
+    }
+    else
+        document.getElementById("configbox").style.display = "none";
+
+    adjustMenuStates(1);
+});
+
+var menuView = document.getElementById("menuview");
+menuView.addEventListener("click", () => {
+    menuViewShown = !menuViewShown;
+    if (menuViewShown) {
+        document.getElementById("viewbox").style.display = "block";
+        document.getElementById("viewbox").style.animation = "movemenu .4s forwards";
+    }
+    else
+        document.getElementById("viewbox").style.display = "none";
+
+    adjustMenuStates(2);
+});
+
+var menuAbout = document.getElementById("menuabout");
+menuAbout.addEventListener("click", () => {
+    menuAboutShown = !menuAboutShown;
+    if (menuAboutShown) {
+        document.getElementById("aboutbox").style.display = "block";
+        document.getElementById("aboutbox").style.animation = "movemenu .4s forwards";
+    }
+    else
+        document.getElementById("aboutbox").style.display = "none";
+
+    adjustMenuStates(3);
+});
+
+let windowShown = false;
+var menuShowPlaylist = document.getElementById("menuShowPlaylist");
+
+menuShowPlaylist.addEventListener("click", () => {
+    windowShown = !windowShown;
+
+    adjustMenuStates(-1);
+});
+
+var menuCoche = [
+    document.getElementById("menucoche0"),
+    document.getElementById("menucoche1"),
+    document.getElementById("menucoche2"),
+    document.getElementById("menucoche3"),
+    document.getElementById("menucoche4"),
+    document.getElementById("menucoche5"),
+    document.getElementById("menucoche6")];
+
+function adjustChecklist() {
+    for (let idx = 0; idx < menuCoche.length; idx++) {
+        if (idx === selectedChecklist)
+            menuCoche[idx].classList.replace("fa-square", "fa-square-check");
+        else
+            menuCoche[idx].classList.replace("fa-square-check", "fa-square");
+    }
+}
+
+var menuNoTransition = document.getElementById("menunotransition");
+var menuNoTransitionCoche = document.getElementById("menunotransitioncoche");
+menuNoTransition.addEventListener("click", () => {
+    adjustMenuStates(-1);
+
+    selectedChecklist = 0;
+    adjustChecklist();
+});
+
+var menuRandomTransition = document.getElementById("menurandomtransition");
+menuRandomTransition.addEventListener("click", () => {
+    adjustMenuStates(-1);
+
+    selectedChecklist = 1;
+    adjustChecklist();
+});
+
+var menuOrderedTransition = document.getElementById("menuorderedtransition");
+menuOrderedTransition.addEventListener("click", () => {
+    adjustMenuStates(-1);
+
+    selectedChecklist = 2;
+    adjustChecklist();
+});
+
+var menuSloMoveTrnasition = document.getElementById("menuslowmovetransition");
+menuSloMoveTrnasition.addEventListener("click", () => {
+    adjustMenuStates(-1);
+
+    selectedChecklist = 3;
+    adjustChecklist();
+});
+
+var menuRollonTransition = document.getElementById("menurollontransition");
+menuRollonTransition.addEventListener("click", () => {
+    adjustMenuStates(-1);
+
+    selectedChecklist = 4;
+    adjustChecklist();
+});
+
+var menuRotateTransition = document.getElementById("menurotatetransition");
+menuRotateTransition.addEventListener("click", () => {
+    adjustMenuStates(-1);
+
+    selectedChecklist = 5;
+    adjustChecklist();
+});
+
+var menuTurnTransition = document.getElementById("menuturntransition");
+menuTurnTransition.addEventListener("click", () => {
+    adjustMenuStates(-1);
+
+    selectedChecklist = 6;
+    adjustChecklist();
+});
+
+var menuEarthSystem = document.getElementById("menuearthsystem");
+menuEarthSystem.addEventListener("click", () => {
+    adjustMenuStates(-1);
+});
+
+var menuLogout = document.getElementById("menulogout");
+menuLogout.addEventListener("click", () => {
+    adjustMenuStates(-1);
+});
+
+/* Deactivate menu when clicking outside */
+
+background.addEventListener("click", () => {
+    if (menuEarthShown || menuConfigShown || menuViewShown || menuAboutShown)
+        adjustMenuStates(-1);
+});
+
+var footerFrame = document.getElementById("footer");
+footerFrame.addEventListener("click", () => {
+    if (menuEarthShown || menuConfigShown || menuViewShown || menuAboutShown)
+        adjustMenuStates(-1);
+});
+
+/* Add window title bar */
+
+document.getElementById('windowbar1').appendChild(addWindowFrame('Hello World'));
+document.getElementById('windowbar2').appendChild(addWindowFrame('Beautifull World', true));
